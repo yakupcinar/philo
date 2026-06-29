2de99136f86e1dc61dfa1848dc5f702208aa9816
ycinarog <jacobcinar03@gmail.com>
2026-06-29 20:59:14 +0300

    Update philo.h

--- philo.h
+++ philo.h
@@ -7,7 +7,6 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-/* Ortak Veri Yapısı (Masa) */
 typedef struct s_data
 {
 	int				philo_count;
@@ -23,7 +22,6 @@ typedef struct s_data
 	pthread_mutex_t	dead_mutex;
 }					t_data;
 
-/* Filozof Veri Yapısı */
 typedef struct s_philo
 {
 	int				id;
@@ -38,29 +36,24 @@ typedef struct s_philo
 	t_data			*data;
 }					t_philo;
 
-// utils.c
 long long			get_current_time(void);
 void				ft_usleep(int time_in_ms);
 long long			time_diff(long long past, long long pres);
 int					check_if_dead(t_data *data);
 int					ft_atoi(const char *nptr);
 
-// init.c
 void				assign_forks(t_philo *philo, t_data *data, int i);
 int					init_mutexes(t_data *data);
 int					init_philos(t_data *data, t_philo **philos);
 int					init_data(t_data *data, int argc, char **argv);
 
-// routine.c
 void				*philosopher_routine(void *arg);
 void				monitor_routine(t_data *data, t_philo *philos);
 void				print_status(t_philo *philo, char *status);
-int	check_if_dead(t_data *data); // Ölüm kontrolü
+int					check_if_dead(t_data *data);
 
-// simulation.c
 int					start_simulation(t_data *data, t_philo *philos);
 
-// leak.c (veya clean_up için dosyan)
 void				clean_up(t_data *data, t_philo *philos);
 
 #endif
